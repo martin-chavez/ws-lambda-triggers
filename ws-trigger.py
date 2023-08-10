49c3d4d69e22d5ab97eaaf46cc1503d31366579c
thomascoleman908 <131822495+thomascoleman908@users.noreply.github.com>
2023-08-10 11:46:13 -0400

    Update ws-trigger.py

--- ws-trigger.py
+++ ws-trigger.py
@@ -1,6 +1,10 @@
 import requests
 def lambda_handler(event, context):
-  url="http://34.194.92.175:5000/changes/" + str(event["idParent"])
-  response = requests.get(url)
-  print(response.text)
-  print(response.status_code, response.reason)
+  try:
+    url="http://44.194.243.124:5000/changes/" + str(event["idParent"])
+    response = requests.get(url)
+    print(response.text) #TEXT/HTML
+    print(response.status_code, response.reason) #HTTP
+  except Exception as e:
+    print("Error occurred while hitting WS API")
+    print(e)
